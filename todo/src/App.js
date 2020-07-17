22ccdc8350ebc78fac7dd7495708d774686bee42
OlgaVinogradova <49756613+OlgaVinogradova@users.noreply.github.com>
2020-07-17 23:33:29 +0300

    create numbers

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { count, length } from './numbers.js';
 
 const sayHi = 'Hello world!';
 const num = 30;
@@ -26,6 +27,9 @@ function App() {
           {isTrue && 'isTrue is true'}
         <p>
           {flag ? 'flag is true' : 'flag is false'}, {undefined}, {null}, {false}, {true}
+        </p>
+        <p>
+          {count*length}
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
