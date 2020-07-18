f521d4522753aba82baebfa9fcb9b41a93783df9
OlgaVinogradova <49756613+OlgaVinogradova@users.noreply.github.com>
2020-07-18 15:40:44 +0300

    add components to app.js and index.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const element = (<div>
+const ItemList = () => (<ul>
+  <li>play with me</li>
+  <li>feed me</li>
+</ul>);
+
+const App = () => (<div>
   <h1 style={{
     color: 'crimson'
     }}>Hello, Olga!
   </h1>
-  <a href="">play with me</a>
+  <ItemList />
 </div>);
 
-export { element };
+export default App;
