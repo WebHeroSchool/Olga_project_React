d4388becc820673c66c7d17d2d08e46fb2718b6c
OlgaVinogradova <49756613+OlgaVinogradova@users.noreply.github.com>
2020-08-02 18:25:59 +0300

    add material UI

--- src/Components/InputItem/InputItem.js
+++ src/Components/InputItem/InputItem.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import styles from './InputItem.module.css';
 
 const InputItem = ()=> (
-  <h2>Важный список</h2>);
-
+  <div>
+    <TextField
+      id="standard-full-width"
+      label="напиши и сделай"
+      placeholder="Новая задача"
+      fullWidth
+      InputLabelProps={{
+        shrink: true,
+      }}
+     />
+  </div>);
 export default InputItem;
