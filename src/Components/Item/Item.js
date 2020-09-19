34a96f6238c7a3b3d9d49ab1b89822d34b6d38bf
OlgaVinogradova <49756613+OlgaVinogradova@users.noreply.github.com>
2020-09-19 15:40:10 +0300

    fix component and style, add remove

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,41 +1,29 @@
-// import React from 'react';
-// import classnames from 'classnames';
-// import styles from './Item.module.css';
-
-// const Item = ({ value, isDone, onClickDone }) => (<span className={
-//   classnames({
-//     [styles.item]: true,
-//     [styles.done]: isDone
-//   })}> 
-//   {value}
-// </span>);
-
-// export default Item;
-
 import React from 'react';
 import classnames from 'classnames';
 import styles from './Item.module.css';
 import Checkbox from '@material-ui/core/Checkbox';
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 
-const Item = ({ value, isDone, onClickDone, id, onClickDelete }) => (<span className = {
-  classnames({
-    [styles.item]: true,
-    [styles.done]: isDone,
-  })}>
-  <div className = {styles.list}>
-    <Checkbox
-       color="secondary"
-       checked={isDone}
-       onClick={() => onClickDone(id)} 
-    />  
-    {value}
-    <DeleteOutlinedIcon
-      onClick={() => onClickDelete(id)}
-      color="secondary"
-      className={styles.delete}
-     />
-  </div>
-</span>);
+const Item = ({ value, isDone, onClickDone, id, onClickDelete }) => (
+  <li className = {styles.list}>
+    <span className = {
+      classnames({
+        [styles.item]: true,
+        [styles.done]: isDone,
+      })}>
+      <Checkbox
+        color="secondary"
+        checked={isDone}
+        onClick={() => onClickDone(id)} 
+       />  
+      {value}
+      <DeleteOutlinedIcon
+        onClick={() => onClickDelete(id)}
+        color="secondary"
+        className={styles.delete}
+       />  
+    </span>
+  </li>
+);
 
 export default Item;
