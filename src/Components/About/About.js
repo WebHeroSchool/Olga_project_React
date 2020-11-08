dde7da453fe2092cfa618a3a2f95c58a302b1a00
OlgaVinogradova <49756613+OlgaVinogradova@users.noreply.github.com>
2020-11-09 00:26:47 +0300

    add  request

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,8 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CardContent from '@material-ui/core/CardContent';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Octokit } from "@octokit/rest";
+
+import styles from './About.module.css';
 
 const About = () => {
+
+  const octokit = new Octokit();
+
+  const login = 'OlgaVinogradova';
+  const initialState = {
+    isLoading: true,
+    reposList: [],
+    name: '',
+    avatarUrl: '',
+    htmlUrl: '',
+    bio: '',
+    error: ''
+  };
+
+  const [isLoading, setIsLoading] = useState(initialState.isLoading);
+  const [reposList, setReposList] = useState(initialState.reposList);
+  const [name, setName] = useState(initialState.name);
+  const [avatarUrl, setAvatarUrl] = useState(initialState.avatarUrl);
+  const [htmlUrl, setHtmlUrl] = useState(initialState.htmlUrl);
+  const [bio, setBio] = useState(initialState.bio);
+  const [error, setError] = useState(initialState.error);
+
+  useEffect(() => {
+    octokit.repos.listForUser({
+      username: login,
+    })
+      .then((repositories) => {
+        setReposList(repositories.data);
+        setIsLoading(false);
+      }).catch((err) => {
+        setError(err);
+        setIsLoading(false);
+      });
+    octokit.users.getByUsername({
+      username: login
+    })
+      .then((userData) => {
+        setName(userData.data.name);
+        setAvatarUrl(userData.data.avatar_url);
+        setHtmlUrl(userData.data.html_url);
+        setBio(userData.data.bio);
+      }).catch((err) => {
+        setError(err);
+      });
+  }, []);
+
   return (
-    <p>About</p>
+    <CardContent>
+      { isLoading ? <CircularProgress /> :
+        <div className={styles.userInfo}>
+          <img className={styles.userAvatar} src={avatarUrl} />
+          <div>
+            <h2><a href={htmlUrl} target='blank'> {name ? name : login} </a></h2>
+            <p><small>{bio}</small></p>
+          </div>
+        </div>}
+      { !isLoading && error ? <span>Ошибка</span> :
+        <ol className={styles.reposList}>
+          <h3 className={styles.text}>Мои репозитории:</h3>
+          {reposList.map((repos) => (
+            <li key={repos.id}>
+              <a href={repos.html_url} target='blank'>
+                {repos.name}
+              </a>
+            </li>))}
+        </ol>}
+    </CardContent >
   );
 };
 
