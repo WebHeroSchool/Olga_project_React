9d62005efb0fd7e176f8988192902fc396d47718
OlgaVinogradova <49756613+OlgaVinogradova@users.noreply.github.com>
2021-01-23 03:01:37 +0300

    Updates

---
+++ precache-manifest.d16c44757850c8115919d6dc87843380.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd69b9dafa3c8848451182fcd5fbce7",
+    "url": "/Olga_ToDo_React/index.html"
+  },
+  {
+    "revision": "9ecf81340b5f6ba1fb26",
+    "url": "/Olga_ToDo_React/static/css/main.191e7ea1.chunk.css"
+  },
+  {
+    "revision": "78ba5ef18f494958a976",
+    "url": "/Olga_ToDo_React/static/js/2.14c6c0af.chunk.js"
+  },
+  {
+    "revision": "ea400bfb7cfb62de59f10ac82e4a6f9d",
+    "url": "/Olga_ToDo_React/static/js/2.14c6c0af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ecf81340b5f6ba1fb26",
+    "url": "/Olga_ToDo_React/static/js/main.a602da0f.chunk.js"
+  },
+  {
+    "revision": "901356db01ddf3c2999d",
+    "url": "/Olga_ToDo_React/static/js/runtime-main.e867244c.js"
+  },
+  {
+    "revision": "a3b344a246a1de75f22227e0e72f5216",
+    "url": "/Olga_ToDo_React/static/media/Star.a3b344a2.svg"
+  },
+  {
+    "revision": "0a1558bf5081fe0d139f0eb344d36182",
+    "url": "/Olga_ToDo_React/static/media/fork.0a1558bf.svg"
+  },
+  {
+    "revision": "4bf03c4a9ab67744db0259f2fc392554",
+    "url": "/Olga_ToDo_React/static/media/vk.4bf03c4a.svg"
+  }
+]);
